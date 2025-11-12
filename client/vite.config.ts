edf61f0b990f4ee22cf1be59c48c5ca7d31b319b
R.Huijts <r.huijts@fontys.nl>
2025-11-12 11:05:19 +0100

    ✨ feat: Update Development Environment Configuration
    - Upgraded Node.js version in Dockerfile from 18-bullseye to 20-bookworm for improved performance and compatibility.
    - Added necessary build tools and Python for node-gyp in Dockerfile.
    - Configured docker-compose.yml to expose ports 3080 for Backend API and 3090 for Frontend Dev Server.
    - Changed Vite server host from 'localhost' to '0.0.0.0' to allow external access during development.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig(({ command }) => ({
   base: '',
   server: {
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3090,
     strictPort: false,
     proxy: {
